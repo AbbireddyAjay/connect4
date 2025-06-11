7e435b2ac2b5d29a89b414a1116cd6d72b4e1973
AbbireddyAjay <158570863+AbbireddyAjay@users.noreply.github.com>
2025-06-11 07:08:32 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+
+
+var playerRed="R";
+var playerYellow ="y";
+var currPlayer = playerRed;
+var gameOver =false;
+var board;
+
+var rows=6;
+var columns=7;
+
+window.onload = function(){
+    board = [];
+}
